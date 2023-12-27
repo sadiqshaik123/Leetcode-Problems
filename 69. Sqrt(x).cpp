5ffdf3b0a31d175f8ccfea99d882dfe263576db8
Sadiq <53291095+sadiqshaik123@users.noreply.github.com>
2023-12-27 10:01:11 +0530

    Create 69. Sqrt(x).cpp

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        double res = 1, pre = 0;
+        while (abs(res - pre) > 1e-6) {
+            pre = res;
+            res = (res + x / res) / 2;
+        }
+        return int(res);
+    }
+};
